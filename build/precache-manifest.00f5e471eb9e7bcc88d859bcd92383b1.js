5cc91a5c9ce4d051c1fc77524f3a5c2c4e812452
opeskola <opeskola@gmail.com>
2019-06-05 00:07:17 +0300

    frontend together with backend

---
+++ build/precache-manifest.00f5e471eb9e7bcc88d859bcd92383b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80fdd9896c85501c2fb6c2e80adc5bb1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68b352d93e89e4b690dc",
+    "url": "/static/css/main.efee2b2a.chunk.css"
+  },
+  {
+    "revision": "1a95d5a327b9fa556408",
+    "url": "/static/js/2.9fde0a89.chunk.js"
+  },
+  {
+    "revision": "68b352d93e89e4b690dc",
+    "url": "/static/js/main.7354e030.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
