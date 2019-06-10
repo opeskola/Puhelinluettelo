6356765424af1ba430358d3c0a83a501bebd6581
opeskola <opeskola@gmail.com>
2019-06-10 23:43:47 +0300

    uibuild

--- mongo.js
+++ mongo.js
@@ -5,11 +5,13 @@ if ( process.argv.length<3 ) {
   process.exit(1)
 }
 
+
+
 const password = process.argv[2]
 const arg_name = process.argv[3]
 const arg_number = process.argv[4]
 
-const url = `mongodb+srv://fullstack:${password}@cluster0-enjmy.mongodb.net/test?retryWrites=true&w=majority`
+const url = `mongodb+srv://fullstack:${password}@cluster0-enjmy.mongodb.net/note-app?retryWrites=true&w=majority`
 
 mongoose.connect(url, { useNewUrlParser: true })
 
@@ -21,13 +23,43 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-const person = new Person({
+// if one parameter is given, the database content are printed to console
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+// else if three parameters are given, the name and number are saved to database
+else if (process.argv.length === 5) {
+  const person = new Person({
     id: Math.floor(100000 * Math.random()),
     name: arg_name,
     number: arg_number,
-})
-
-person.save().then(response => {
-    console.log('person saved!');
+  })
+  
+  person.save().then(response => {
+    console.log('added', arg_name, 'number', arg_number, 'to phonebook');
     mongoose.connection.close();
   })
+}
+
+else {
+  console.log('amount of parameters not valid');
+  mongoose.connection.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+  
